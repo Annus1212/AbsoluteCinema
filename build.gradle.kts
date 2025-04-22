0d7ee4192684f2be04f80cf7edad8d3e3227eca1
Raza <razaalirizvi4@gmail.com>
2025-04-23 02:17:45 +0500

    complete workinng landing page, still in need of connection with db or api

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,8 @@ group = "com.absolutecinema"
 version = "1.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_24
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
@@ -40,7 +41,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "21"
+        jvmTarget = "17"
     }
 }
 
